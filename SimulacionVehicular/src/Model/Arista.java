68d106d2b2564cc7d959085fb5c4fc6207b22f48
Aldokler <xdavidpastor@gmail.com>
2023-09-02 22:38:19 -0600

    quería aportar algo ugu

---
+++ SimulacionVehicular/src/Model/Arista.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Aldokler
+ */
+public class Arista {
+    
+    Nodo inicio, fin;
+    int distancia;
+
+    public Arista(Nodo inicio, Nodo fin, int distancia) {
+        this.inicio = inicio;
+        this.fin = fin;
+        this.distancia = distancia;
+    }
+
+    public Nodo getInicio() {
+        return inicio;
+    }
+
+    public Nodo getFin() {
+        return fin;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+
+    public void setInicio(Nodo inicio) {
+        this.inicio = inicio;
+    }
+
+    public void setFin(Nodo fin) {
+        this.fin = fin;
+    }
+
+    public void setDistancia(int distancia) {
+        this.distancia = distancia;
+    }
+    
+    
+}
---
+++ SimulacionVehicular/src/Model/Car.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Aldokler
+ */
+public class Car {
+    
+    String Identifier;
+    int Velocidad;
+    Nodo destino;
+    boolean estado;
+    int x, y;
+    Object imagen;
+
+    public Car(String Identifier, Nodo destino, int x, int y) {
+        this.Identifier = Identifier;
+        this.destino = destino;
+        this.x = x;
+        this.y = y;
+        this.Velocidad = 10;
+        this.estado = true;
+    }
+
+    public String getIdentifier() {
+        return Identifier;
+    }
+
+    public int getVelocidad() {
+        return Velocidad;
+    }
+
+    public Nodo getDestino() {
+        return destino;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Object getImagen() {
+        return imagen;
+    }
+
+    public void setVelocidad(int Velocidad) {
+        this.Velocidad = Velocidad;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+}
