97217b5e5587f00c448dfe3de29301f44e1a750d
Aldokler <xdavidpastor@gmail.com>
2023-09-18 18:02:52 -0600

    .

--- Install.sh
+++ Install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 dnf install java-17-openjdk.x86_64
 dnf install java-17-openjdk-devel.x86_64
+
+exit
--- Run.sh
+++ Run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd SimulacionVehicular/build/classes/View
 java Main.class
+
+exit
